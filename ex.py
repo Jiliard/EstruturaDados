5ae8b8b390651954d0364368c45acce63f29a409
Jiliard <108362526+Jiliard@users.noreply.github.com>
2024-04-16 16:06:48 -0300

    Add files via upload

--- ex.py
+++ ex.py
@@ -3,6 +3,7 @@ class objeto():
     def __init__(self, dado):
         self.__dado = dado
         self.__prox = None
+        self.__anter = None
     
     def getdado(self):
         return self.__dado
@@ -16,39 +17,89 @@ class objeto():
     def setprox(self, prox):
         self.__prox = prox
 
+    def getAnter(self):
+        return self.__anter
+    
+    def setAnter(self, anter):
+        self.__anter = anter
+
 
 class lista():
 
     def __init__(self):
         self.__prim = None
         self.__ult = None
-        self.__cont = 0
+        self.__cursor = None
+    
+    def buscar(self, ref):
+        self.__irParaOPrimeiro()
+
+        while self.__cursor.getDado() != ref:
+            self.__avancarKPosicoes(1)
+
+        return True
+    
+    def AcessarAtual(self):
+        return self.__cursor.getdado()
+
+    def ExcluirAtual (self):
+        #falta definir onde está o cursor, outro método 'buscar'
+        prox = self.__cursor.getProx()
+        ant = self.__cursor.getAnter()
+        prox.setAnter(ant)
+        ant.setProx(prox)
+        self.__cursor = prox
+
+    #feito
+    def __avancarKPosicoes(self, k):
+        for i in range(k):
+            self.__cursor = self.__cursor.getProx()
+
+    #feito
+    def __retrocederKPosicoes(self, k):
+        for i in range(k):
+            self.__cursor = self.__cursor.getAnter()
+
+    #feito
+    def __irParaOPrimeiro(self):
+        while self.__cursor.getAnter() != None:
+            self.__cursor = self.__cursor.getAnter()
+
+    #feito
+    def __irParaOUltimo(self):
+        while self.__cursor.getProx() != None:
+            self.__cursor = self.__cursor.getProx()
+
+    def __Vazio(self) -> bool:
+        if self.__prim == None:
+            return True
+        return False
     
     #feito
     def InserirComoPrimeiro(self, el):
         novo = objeto(el)
 
-        if self.__cont == 0:
+        if self.__prim == None:
             self.__ult = novo
+            self.__cursor = novo
 
         else:
             novo.setprox(self.__prim)
 
         self.__prim = novo
-        self.__cont += 1
 
     #feito
     def InserirComoUltimo(self, el):
         novo = objeto(el)
 
-        if self.__cont == 0:
+        if self.__ult == None:
             self.__prim = novo
+            self.__cursor = novo
 
         else:
             self.__ult.setprox(novo)
 
         self.__ult = novo
-        self.__cont += 1
 
     #feito    
     def InserirNaPosicao(self, el, pos):
@@ -63,7 +114,6 @@ class lista():
 
         novo.setprox(iterador.getprox()) 
         iterador.setprox(novo)
-        self.__cont += 1
 
     #feito
     def InserirAntesDe(self, el, ref):
@@ -75,7 +125,6 @@ class lista():
 
         novo.setprox(iterador.getprox()) 
         iterador.setprox(novo)
-        self.__cont += 1
 
     #feito
     def InserirDepoisDe(self, el, ref):
@@ -87,17 +136,15 @@ class lista():
 
         novo.setprox(iterador.getprox()) 
         iterador.setprox(novo)
-        self.__cont += 1
 
     #feito
     def RemoverPrimeiro(self):
-        if self.__cont == 0:
+        if self.__Vazio():
             print("EXCEÇÃO")
 
         else:
             aux = self.__prim.getprox()
             self.__prim = aux
-            self.__cont -= 1
 
     #feito
     def RemoverUltimo(self):
@@ -107,38 +154,31 @@ class lista():
             iterador = iterador.getprox()
 
         iterador = self.__ult
-        self.__cont -= 1
 
     #feito
     def AcessaPrimeiro(self):
-        return self.__prim.getdado().getdado()
+        return self.__prim.getdado()
     
     #feito
     def AcessaUltimo(self):
-        return self.__ult.getdado().getdado()
+        return self.__ult.getdado()
     
     #feito
     def MostrarTudo(self):
         iterador = self.__prim
-        atual = 1
 
-        while atual <= self.__cont:
-            print(iterador.getdado().getdado())
+        while True:
+            print(iterador.getdado())
+            if iterador.getprox() == None: break
             iterador = iterador.getprox()
-            atual +=1
-
-p1 = objeto(1)
-p2 = objeto(2)
-p3 = objeto(3)
-p4 = objeto(4)
-p5 = objeto(5)
 
 lista = lista()
 
-lista.InserirComoUltimo(p1)
+lista.InserirComoUltimo(1)
 
-lista.InserirComoUltimo(p2)
+lista.InserirComoUltimo(2)
 
-lista.InserirComoUltimo(p3)
+lista.InserirComoUltimo(3)
 
 lista.MostrarTudo()
+
