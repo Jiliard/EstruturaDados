979579b68cc3ef6199a101a1556633712d336f6f
Jiliard <jiliardmaipeifer172@gmail.com>
2024-04-21 15:51:43 -0300

    Update ex.py

--- ex.py
+++ ex.py
@@ -26,27 +26,13 @@ class objeto():
 
 class lista():
 
-    def __init__(self):
+    def __init__(self, tamanho = 30):
         self.__prim = None
         self.__ult = None
         self.__cursor = None
-
-    def buscar(self, ref):
-        self.__irParaOPrimeiro()
-
-        while self.__cursor.getDado() != ref:
-            self.__avancarKPosicoes(1)
-
-        return True
-
-    def ExcluirAtual (self):
-        #falta definir onde está o cursor, outro método 'buscar'
-        prox = self.__cursor.getProx()
-        ant = self.__cursor.getAnter()
-        prox.setAnter(ant)
-        ant.setProx(prox)
-        self.__cursor = prox
-
+        self.__tamanho = tamanho
+        self.__quantidade = 0
+      
     #feito
     def __avancarKPosicoes(self, k):
         for i in range(k):
@@ -72,6 +58,15 @@ class lista():
             return True
         return False
     
+    def __Cheio(self) -> bool:
+        if self.__quantidade >= self.__tamanho:
+            return True
+        return False
+    
+    #feito
+    def acessarAtual(self):
+        return self.__cursor.getDado()
+    
     #feito
     def InserirComoPrimeiro(self, el):
         novo = objeto(el)
@@ -84,6 +79,8 @@ class lista():
             novo.setprox(self.__prim)
 
         self.__prim = novo
+
+        self.__quantidade += 1
 
     #feito
     def InserirComoUltimo(self, el):
@@ -98,59 +95,124 @@ class lista():
 
         self.__ult = novo
 
+        self.__quantidade += 1
+
     #feito    
     def InserirNaPosicao(self, el, pos):
-        novo = objeto(el)
+        self.__irParaOPrimeiro()
+        self.__avancarKPosicoes(pos)
+        self.InserirAntesDoAtual(el)
 
-        iterador = self.__prim
-        atual = 2
-
-        while atual < pos:
-            iterador = iterador.getprox()
-            atual +=1
-
-        novo.setprox(iterador.getprox()) 
-        iterador.setprox(novo)
+        self.__quantidade += 1
 
     #feito
-    def InserirAntesDe(self, el, ref):
+    def InserirAntesDoAtual(self, el):
         novo = objeto(el)
-        iterador = self.__prim
 
-        while iterador.getprox().getdado().getdado() != ref:
-            iterador = iterador.getprox()
+        novo.setAnter(self.__cursor.getAnter())
+        novo.setProx(self.__cursor)
 
-        novo.setprox(iterador.getprox()) 
-        iterador.setprox(novo)
+        self.__cursor.getAnter().setProx(novo)
+        self.__cursor.setAnter(novo)
+
+        self.__quantidade += 1
 
     #feito
-    def InserirDepoisDe(self, el, ref):
+    def InserirDepoisDoAtual(self, el):
         novo = objeto(el)
-        iterador = self.__prim
 
-        while iterador.getdado().getdado() != ref:
-            iterador = iterador.getprox()
+        novo.setAnter(self.__cursor)
+        novo.setProx(self.__cursor.getProx())
 
-        novo.setprox(iterador.getprox()) 
-        iterador.setprox(novo)
+        self.__cursor.getPro().setAnter(novo)
+        self.__cursor.setProx(novo)
+
+        self.__quantidade += 1
 
     #feito
-    def RemoverPrimeiro(self):
+    def ExcluirAtual(self):
+        self.__cursor.getAnter().setProx(self.__cursor.getProx())
+        self.__cursor.getProx().setAnter(self.__cursor.getAnter())
+
+        self.__avancarKPosicoes(1)
+
+        self.__quantidade -= 1
+
+    #feito
+    def ExcluirPrim(self):
         if self.__Vazio():
             print("EXCEÇÃO")
 
         else:
-            aux = self.__prim.getprox()
-            self.__prim = aux
+            self.__prim = self.__prim.getprox()
+            if not self.__Vazio():
+                self.__prim.setAnt(None)
+
+        self.__quantidade -= 1
 
     #feito
-    def RemoverUltimo(self):
-        iterador = self.__prim
+    def ExcluirUlt(self):
+        if self.__Vazio():
+            print("Exceção")
+        else:
+            self.__ult = self.__ult.getAnter()
+            if not self.__Vazio:
+                self.__ult.setProx(None)
 
-        while iterador.getprox() != self.__ult:
-            iterador = iterador.getprox()
+        self.__quantidade -= 1
 
-        iterador = self.__ult
+    #feito
+    def ExcluirElemento(self, el):
+        self.__irParaOPrimeiro
+        while True:
+            if self.__cursor.getDado() == el:
+                self.ExcluirAtual()
+
+            if self.__cursor == self.__ult:
+                break
+
+            self.__avancarKPosicoes(1)
+
+        self.__quantidade -= 1
+
+    #feito
+    def ExcluirDaPos(self, pos):
+        self.__irParaOPrimeiro()    
+        self.__avancarKPosicoes(pos)
+
+        self.ExcluirAtual()
+
+        self.__quantidade -= 1
+
+    def Buscar(self, el):
+        self.__irParaOPrimeiro()
+
+        while True:
+            if self.__cursor.getDado() == el:
+                return True
+            
+            if self.__cursor == self.__ult:
+                break
+            
+            self.__avancarKPosicoes(1)
+
+        return False
+    
+    #feito
+    def PosicaoDe(self, el) -> int:
+        self.__irParaOPrimeiro()
+        contador = 0;
+        while True:
+            if self.__cursor.getDado() == el:
+                return contador
+            
+            if self.__cursor == self.__ult:
+                print("Excecao")
+                break
+            
+            self.__avancarKPosicoes(1)
+            contador += 1
+        
 
     #feito
     def AcessaPrimeiro(self):
@@ -178,4 +240,3 @@ lista.InserirComoUltimo(2)
 lista.InserirComoUltimo(3)
 
 lista.MostrarTudo()
-
